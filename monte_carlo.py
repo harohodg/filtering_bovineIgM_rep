4071ccce4bd81246c29e541158be670549c804f9
Harold Hodgins <hhodgins@uwaterloo.ca>
2024-04-08 18:49:39 -0400

    Updated monte carlo with cleaner argument parsing and optional output file

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,36 +1,54 @@
 #!/usr/bin/python3
-#First run module load scipy-stack/2023a
-#Then run python3 monte_carlo.py <input_file> <sample_size> <number_of_samples> [<threshold>]
-#Returns mean and std of the percentage of values in each sample >= the threshold if a threshold is provided
-#Otherwise returns the mean and std of the mean of each sample
 
-import sys, random
+#First run module load StdEnv/2020 scipy-stack/2023a
+
+DESCRIPTION = """
+Prints the mean and std of the percentage of values in each sample >= the threshold if a threshold is provided
+Otherwise prints the mean and std of the mean of each sample
+The intermediary values are saved to an optional output file.
+"""
+
+DEFAULT_SAMPLE_SIZE       = 1000
+DEFAULT_NUMBER_OF_SAMPLES = 1000
+
+import sys, random, argparse
 import numpy as np
+from numpy import genfromtxt
 
-inputFile   = sys.argv[1]
-sample_size = int(sys.argv[2])
-num_samples = int(sys.argv[3])
+parser = argparse.ArgumentParser(
+    prog="monte_carlo.py",
+    description=DESCRIPTION,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
 
-threshold   = int(sys.argv[4]) if len(sys.argv) == 5 else None
+parser.add_argument('input_file')
+parser.add_argument('-s', '--sample_size',       type=int, default=DEFAULT_SAMPLE_SIZE,       help='# of lengths in each samples')
+parser.add_argument('-n', '--number_of_samples', type=int, default=DEFAULT_NUMBER_OF_SAMPLES, help='# of samples taken')
+parser.add_argument('-t', '--threshold',         type=int, help='length threshold')
+parser.add_argument('-o', '--output_file')
 
-with open( inputFile, 'r') as f:
-    first_line = f.readline()
-    data = [ float(line) for line in f ]
+args = parser.parse_args()
 
-observed_length = []
+data = list( genfromtxt(args.input_file, delimiter=',') )
 
-for sample_number in range( num_samples ):   
-    sample      = random.sample( data, sample_size )
-    observed_length.append(0)
+observed_lengths = []
+
+for sample_number in range( args.number_of_samples ):   
+    sample = random.sample( data, args.sample_size )
+    observed_lengths.append(0)
     
-    if threshold is not None:
+    if args.threshold is not None:
         for value in sample:
-            observed_length[-1] += value >= threshold
-        observed_length[-1] = (observed_length[-1]/sample_size)*100
+            observed_lengths[-1] += value >= args.threshold
+        observed_lengths[-1] = (observed_lengths[-1]/args.sample_size)*100
     else:
-        observed_length[-1] = np.mean(sample)
-       
-observed_length_mean = np.mean( observed_length )
-observed_length_std  = np.std( observed_length )
+        observed_lengths[-1] = np.mean(sample)
 
-print( f'{observed_length_mean:.4f},{observed_length_std:.4f},{sample_size},{num_samples},{threshold}' )
+if args.output_file is not None:
+    np.savetxt(args.output_file, observed_lengths, delimiter=",", fmt='%.4f')
+       
+observed_length_mean = np.mean( observed_lengths )
+observed_length_std  = np.std(  observed_lengths )
+
+
+print( f'{observed_length_mean:.4f},{observed_length_std:.4f},{args.sample_size},{args.number_of_samples},{args.threshold}' )
